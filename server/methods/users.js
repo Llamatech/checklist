915cd2888ba5498bbfe90c9f1d608326726e0b5a
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-20 14:03:40 -0500

    Added Facebook and Google OAuth services

--- server/methods/users.js
+++ server/methods/users.js
@@ -1,9 +1,31 @@
 /* global Meteor */
 
+const _getEmailFromService = ( services ) => {
+    for ( let service in services ) {
+        let current = services[ service ];
+        return service === 'twitter' ? current.screenName : current.email;
+    }
+};
+
 Meteor.methods({
     'getUserData': () => {
     // console.log("Ah?");
     // console.log(Meteor.userId());
         return Meteor.users.findOne({_id:Meteor.userId()});
+    },
+    'getUserEmail': () => {
+        let user = Meteor.users.findOne({_id: Meteor.userId()});
+        let emails   = user.emails,
+            services = user.services;
+
+        if ( emails ) {
+            return emails[ 0 ].address;
+        } else if ( services ) {
+            return _getEmailFromService( services );
+        } else {
+            return user.profile.name;
+        }
     }
 });
+
+
