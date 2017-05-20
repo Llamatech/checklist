ee5078c81c9ceadb23796999d05d4df540336bb9
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-20 16:39:26 -0500

    Added basic Facebook and Google OAuth authentication

---
+++ imports/ui/navbar.jsx
@@ -0,0 +1,109 @@
+/* eslint-disable no-undef,
+ no-unused-vars */
+import React, {PropTypes} from 'react';
+import {
+    Navbar,
+    NavItem,
+    NavDropdown,
+    MenuItem,
+    Nav,
+    FormGroup,
+    FormControl,
+    Button,
+    Modal
+} from 'react-bootstrap';
+import LoginButton from './loginbtn.jsx';
+// import PForm from './proyForm';
+
+class Navib extends React.Component {
+    buscar(term) {
+        this.props.buscar(term);
+        //aqui se llama para atras a lo que llame a la db
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showModal: false,
+            showSearch: false,
+            alert: false
+        };
+    }
+
+    modalClose() {
+        this.setState({showModal: false});
+    }
+
+    modalOpen() {
+        if (Meteor.user()) {
+            this.setState({showModal: true});
+        } else {
+            this.setState({alert: true});
+        }
+    }
+
+    close() {
+        this.setState({alert: false});
+    }
+
+    searchClose() {
+        this.setState({showSearch: false});
+    }
+
+    searchOpen() {
+        this.setState({showSearch: true});
+    }
+
+    render() {
+        return (
+            <div>
+              <Navbar collapseOnSelect className="navbar-fixed-top">
+                <Navbar.Header>
+                  <Navbar.Brand>
+                    <a className="logo" onClick={()=>this.props.backHome()}><img src="http://duckit.margffoy-tuay.com/static/checklist.svg" alt="logo" width="20px" style={{float: 'left', 'marginRight': '5px'}}></img>   Checklist</a>
+                  </Navbar.Brand>
+                  <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                  <Nav>
+                    <NavItem className="newProj" onClick={this.modalOpen.bind(this)}>
+                      <i className="fa fa-plus fa-lg fa-inverse "></i>    Add new project
+                    </NavItem>
+                  </Nav>
+                  <Navbar.Form pullLeft>
+                    <FormGroup>
+                      <FormControl type="text" aria-label="Search" placeholder="Search" onChange={(event) => this.buscar(event.target.value)}/>
+                    </FormGroup>
+                    {' '}
+                    {Meteor.user() ? null :
+                      <LoginButton icon='fa-facebook'
+                                   service='Facebook'
+                                   login={this.props.login}/>}
+                    {Meteor.user() ? null :
+                      <LoginButton icon='fa-google'
+                                   service='Google'
+                                   login={this.props.login}/>}
+                  </Navbar.Form>
+                  {Meteor.user()?
+                  <Nav>
+                  <NavDropdown title={Meteor.user().profile.name} id="basic-nav-dropdown" className="newProj">
+                    <MenuItem onClick={()=>this.props.openMine()}>My projects</MenuItem>
+                    <MenuItem divider />
+                    <MenuItem onClick={() => this.props.logout()}>Logout</MenuItem>
+                  </NavDropdown>
+                </Nav>:null
+                }
+                </Navbar.Collapse>
+              </Navbar>
+              <Modal show={this.state.alert} onHide={()=>this.close()}>
+                <Modal.Header closeButton>
+                  You must be logged in to add a new project
+                </Modal.Header>
+
+              </Modal>
+            </div>
+        );
+    }
+}
+
+export default Navib;
