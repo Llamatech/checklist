6f47b2a7ee7dd36b67fd0f29e3a1ecf9cd5caa39
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-05-23 20:25:17 -0500

    Groups API is now fully tested

--- imports/api/groups.test.js
+++ imports/api/groups.test.js
@@ -1,8 +1,9 @@
 /* eslint-env mocha */
 
 import {Meteor} from 'meteor/meteor';
-import { assert } from 'meteor/practicalmeteor:chai';
+import { assert, expect } from 'meteor/practicalmeteor:chai';
 import {Groups} from './groups.js';
+import getEmailFromService from '../../server/methods/users.js';
 
 if(Meteor.server)
 {
@@ -22,9 +23,18 @@ if(Meteor.server)
             Groups.insert({
                 name: 'DefaultGroup',
                 description: 'You shouldn\'t be looking at this',
-                owner: 'llama2@llama.com',
+                owner: 'alpaca@alpaca.com',
                 members: []
             });
+            Meteor.user = function() {
+                return {
+                    'services': {
+                        'facebook': {
+                            'email': 'llama@llama.com'
+                        }
+                    }
+                };
+            };
         });
 
         it('Should create a group', function() {
@@ -34,10 +44,76 @@ if(Meteor.server)
                 owner: 'llama@llama.com',
                 members: []
             };
+            // console.log(getEmailFromService(Meteor.user().services));
 
             Meteor.call('group.insert', {group}, function() {
                 const result = Groups.find({});
                 assert.equal(result.count(), 2);
+            });
+        });
+
+        it('Should not delete a group if current user differs from its actual owner', function() {
+            const group = Groups.find({}).fetch()[0];
+            Meteor.call('group.delete', {groupId: group['_id']}, function(err, res) {
+                const result = Groups.find({});
+                expect(result.count()).to.equal(1);
+                assert.equal(err.reason, 'Cannot delete this group because you are not its owner');
+            });
+        });
+
+        it('Should delete a group', function() {
+            var group = {
+                name: 'SomeGroup',
+                description: 'Some description',
+                owner: 'llama@llama.com',
+                members: []
+            };
+
+            Meteor.call('group.insert', {group}, function() {
+                const result = Groups.find({
+                    'owner': getEmailFromService(Meteor.user().services)
+                }).fetch()[0];
+                Meteor.call('group.delete', {groupId: result['_id']}, function() {
+                    const totalGroups = Groups.find({}).count();
+                    assert.equal(totalGroups, 1);
+                });
+            });
+        });
+
+        it('Should add an user to a group (Owner)', function() {
+            Meteor.user = function() {
+                return {
+                    'services': {
+                        'facebook': {
+                            'email': 'alpaca@alpaca.com'
+                        }
+                    }
+                };
+            };
+
+            let user = {
+                name: 'Llama',
+                email: 'llama@llama.com'
+            }
+
+            const group = Groups.find({}).fetch()[0];
+            Meteor.call('group.addUser', {groupId: group['_id'], user: user}, function() {
+                const result = Groups.find({}).fetch()[0];
+                assert.equal(JSON.stringify(user), JSON.stringify(result.members[0]));
+            });
+        });
+
+        it('Should not add an user to a group (Not Owner)', function() {
+            let user = {
+                name: 'Llama',
+                email: 'llama@llama.com'
+            }
+
+            const group = Groups.find({}).fetch()[0];
+            Meteor.call('group.addUser', {groupId: group['_id'], user: user}, function(err, res) {
+                const result = Groups.find({}).fetch()[0];
+                assert.equal(result.members.length, 0);
+                assert.equal(err.reason, 'Cannot add an user to this group because you are not its owner');
             });
         });
     });
